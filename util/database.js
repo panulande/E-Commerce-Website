3b3ee9cfa7a920b42333ea4903353213031ad5dd
panda <panulande2003@gmail.com>
2024-06-05 16:18:33 +0530

    using sequelize in postAddProducts controller

--- util/database.js
+++ util/database.js
@@ -1,10 +1,4 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('node-complete', 'root', 'Panu@2003', {dialect: 'mysql', host: 'localhost'});
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'Panu@2003'
-});
-
-module.exports = pool.promise();
+module.exports = sequelize;
