be1aab756548b845f773980b3f18f68a4b5f8b0c
panda <panulande2003@gmail.com>
2024-06-20 22:02:59 +0530

    authentication working properly

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,35 +1,139 @@
+const nodemailer = require('nodemailer');
+const Recipient = require('mailersend').Recipient
+const EmailParams = require('mailersend').EmailParams
+const MailerSend = require('mailersend').MailerSend
+const Sender = require('mailersend').Sender
+const crypto = require('crypto');
+const bcrypt = require('bcryptjs');
+
+
+const mailerSendConfig = {apiKey: 'mlsn.c59908742ca4fc2711df6f7a713e8dc26da5d8b02c5a7bb7e509aaa526242e21'}
+const mailerSend = new MailerSend(mailerSendConfig)
+
+
+// const Mailjet = require('node-mailjet');
+// const mailjet = Mailjet.apiConnect(
+//   process.env.MJ_APIKEY_PUBLIC || 'b5b3c08c169214e2d8576aecea22f2d1',
+//   process.env.MJ_APIKEY_PRIVATE || '7fe7e37500418ae08d5b12a877822b0f',
+// );
 const User = require('../models/user');
+const user = require('../models/user');
+
+
 
 exports.getLogin = (req, res, next) => {
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: false
+    errorMessage: message
   });
 };
 
 exports.getSignup = (req, res, next) => {
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
   res.render('auth/signup', {
     path: '/signup',
     pageTitle: 'Signup',
-    isAuthenticated: false
+    errorMessage: message
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email: email })
     .then(user => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      req.session.save(err => {
-        console.log(err);
-        res.redirect('/');
-      });
+      if (!user) {
+        req.flash('error', 'Invalid email or password.');
+        return res.redirect('/login');
+      }
+      bcrypt
+        .compare(password, user.password)
+        .then(doMatch => {
+          if (doMatch) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            return req.session.save(err => {
+              console.log(err);
+              res.redirect('/');
+            });
+          }
+          req.flash('error', 'Invalid email or password.');
+          res.redirect('/login');
+        })
+        .catch(err => {
+          console.log(err);
+          res.redirect('/login');
+        });
     })
     .catch(err => console.log(err));
 };
 
-exports.postSignup = (req, res, next) => {};
+exports.postSignup = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+
+  User.findOne({ email: email })
+    .then(userDoc => {
+      if (userDoc) {
+        req.flash(
+          'error',
+          'E-Mail exists already, please pick a different one.'
+        );
+        return res.redirect('/signup');
+      }
+
+      return bcrypt.hash(password, 12);
+    })
+    .then(hashedPassword => {
+      const user = new User({
+        email: email,
+        password: hashedPassword,
+        cart: { items: [] }
+      });
+      return user.save();
+    })
+    .then(result => {
+      res.redirect('/login');
+
+      const sentFrom = new Sender('MS_Nx2TRU@trial-neqvygmev7z40p7w.mlsender.net', 'Pranav Lande');
+
+      const recipients = [new Recipient(req.body.email)];
+
+      const emailParams = new EmailParams()
+          .setFrom(sentFrom)
+          .setTo(recipients)
+          .setSubject('Password Reset')
+          .setHtml(`<p>You have successfully signed up</p>
+                      `)
+          .setText("Greetings from the team, you got this message through MailerSend.");
+      
+
+
+
+
+      return mailerSend.email.send(emailParams);
+    })
+    .then(result => {
+      const { Status } = result.body;
+      console.log('Email status:', Status);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
 
 exports.postLogout = (req, res, next) => {
   req.session.destroy(err => {
@@ -37,3 +141,110 @@ exports.postLogout = (req, res, next) => {
     res.redirect('/');
   });
 };
+
+exports.getReset = (req, res, next) => {
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+  res.render('auth/reset', {
+    path: '/reset',
+    pageTitle: 'Reset Password',
+    errorMessage: message
+  });
+};
+
+exports.postReset = (req, res, next)=>{
+  crypto.randomBytes(32,(err, buffer)=>{
+    if(err){
+      console.log(err);
+      return res.redirect('/reset');
+    }
+    const token = buffer.toString('hex'); //buffer will store hex values, tostring needed that to convert those hex values to ascii characters
+    User.findOne({email:req.body.email})
+    .then(user => {
+      if(!user){
+        req.flash('error', 'NO ACCOUNT WITH SUCH USERNAME');
+        return res.redirect('/reset');
+      }
+      user.resetToken = token;
+      user.resetTokenExpiration = Date.now() + 3600000;
+      return user.save();
+    }).then(result => {
+        res.redirect('/');
+  
+        const sentFrom = new Sender('MS_Nx2TRU@trial-neqvygmev7z40p7w.mlsender.net', 'Pranav Lande');
+  
+        const recipients = [new Recipient(req.body.email)];
+  
+        const emailParams = new EmailParams()
+            .setFrom(sentFrom)
+            .setTo(recipients)
+            .setSubject('Password Reset')
+            .setHtml(`<p>You requested a password reset</p>
+                      <p>Click this <a href="http://localhost:3000/reset/${token}">Link</a> to set a new Password<p>
+                      <p>If that doesn't work please copy and paste the link below in your browser</p>
+                      <p>http://localhost:3000/reset/${token}</p>
+                        `)
+            .setText("Greetings from the team, you got this message through MailerSend.");
+        
+  
+  
+  
+  
+        return mailerSend.email.send(emailParams);
+      
+    })
+  }); 
+}
+
+exports.getNewPassword = (req, res, next) =>{
+  const token = req.params.token;
+  User.findOne({resetToken: token, resetTokenExpiration: {$gt: Date.now()}})
+  .then(user => {
+    res.render('auth/new-password', {
+      path: '/new-password',
+      pageTitle: 'New Password',
+      errorMessage: message,
+      userId: user._id.toString(),
+      passwordToken: token,
+    });
+  })
+  .catch(err =>{
+    console.log(err);
+  });
+
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+
+
+}
+
+exports.postNewPassword = (req, res, next) =>{
+  const newPassword = req.body.password;
+  const userId = req.body.userId;
+  const passwordToken = req.body.passwordToken;
+
+  User.findOne({resetToken: passwordToken, resetTokenExpiration: {$gt: Date.now()}, _id: userId})
+  .then(user =>{
+    resetUser = user;
+    return bcrypt.hash(newPassword, 12);
+  }).then(hashedPassword =>{
+    resetUser.password = hashedPassword;
+    resetUser.resetToken = undefined;
+    resetUser.resetTokenExpiration = undefined;
+    return resetUser.save();
+  }
+
+  ).then(result =>{
+    res.redirect('/login')
+  }).catch(err => {
+    console.log(err);
+  })
+}
